8822a72b1a614e6a709a78564c52a1670141af09
谭健青Windows <574469551@qq.com>
2019-03-23 13:47:19 +0800

    test5

--- Chen-Timer/js/sercvei_worker.js
+++ Chen-Timer/js/sercvei_worker.js
@@ -3,7 +3,7 @@ if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
         navigator.serviceWorker
             // github Page
-            .register('/JasonGF-Timer/sw.js', { scope: '/' })
+            .register('sw.js', { scope: '/' })
             .then(function (registration) {
             // 注册成功
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
